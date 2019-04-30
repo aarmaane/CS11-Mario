6c80c6bc54ed0b1f6f4c1f2a8856957485e1f25e
aarmaane <38818203+aarmaane@users.noreply.github.com>
2019-04-30 09:19:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from pygame import *
+from random import *
+from os import *
 # Starting up pygame and necessary components
+environ['SDL_VIDEO_CENTERED'] = '1'
 init()
 size = width, height = 800, 600
 screen = display.set_mode(size)
+# Declaring colours
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-
+# Declaring functions
 running = True
 while running:
     for evt in event.get():

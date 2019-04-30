423622cfe5166e34a0ea183d0f207fbea0597dd1
Ket93 <38818395+Ket93@users.noreply.github.com>
2019-04-30 09:25:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+
+# Declaring variables
+
+menu = "game"
+
 # Declaring main functions
+
 def game():
     pass
 

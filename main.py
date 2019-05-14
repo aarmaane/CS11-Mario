853e3edea0f22c2adff8bde4c24b6b5583d5da96
aarmaane <aarmaane@gmail.com>
2019-05-14 09:11:28 -0400

    More bricks

--- main.py
+++ main.py
@@ -213,6 +213,7 @@ def checkCollide(mario, rectLists):
                     mario[VX] = 0
 
 
+
 # Declaring loading functions
 
 def loadFile(targetFile):
@@ -259,7 +260,7 @@ def game():
         moveSprites(marioPos, marioSprites, marioFrame)
         checkCollide(marioPos, rectList)
         drawScene(backgroundPics[levelNum], backPos, marioPos, marioSprites, marioFrame, rectList)
-        #print(RECTFINDER[0] - backPos, RECTFINDER[1], mx - RECTFINDER[0], my - RECTFINDER[1] )
+        print(RECTFINDER[0] - backPos, RECTFINDER[1], mx - RECTFINDER[0], my - RECTFINDER[1] )
         fpsCounter.tick(60)
     return "menu"
 

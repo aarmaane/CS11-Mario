bc3f788ff6cac805774a3b9140ddc56c88caae1a
Armaan Randhawa <aarmaane@gmail.com>
2019-05-21 21:34:17 -0400

    Lots of comments

--- main.py
+++ main.py
@@ -24,14 +24,19 @@ marioFont = font.Font("assets/fonts/marioFont.ttf", 18)
 page = "menu"
 fpsCounter = time.Clock()
 marioPos = [40, 496, 0, 0, "Right", 0]  # X, Y, VX, VY, direction, state
-    # onGround: Boolrean to see fi mario is on a solid ground
-    # jumpFrames: Variable to keep track of frames user has held space for
-    # inGround: Boolean to see if mario has fallen through the floor
-    # state: 0 for small mario, 1 for big mario
-    # onPlatform: Boolean to see if mario's last position was on a platform
+# X, Y: Variables to keep track of mario's position on screen
+# VX, VY:  Variables to keep track of mario's X and Y velocity
+# direction: Variable to keep track of the direction mario is facing
+# state: 0 for small mario, 1 for big mario
 marioStats = [True, 0, False, False, False, False] # onGround, jumpFrames, inGround, isCrouch, onPlatform, isFalling
+# onGround: Boolean to see if mario is on a solid ground
+# jumpFrames: Variable to keep track of frames user has held space for
+# inGround: Boolean to see if mario has fallen through the floor
+# isCrouch: Boolean to see if mario is crouching
+# onPlatform: Boolean to see if mario's last position was on a platform
+# isFalling: Boolean to see if mario has stopped jumping and should fall
 marioFrame = [0, 0] # List to keep track of mario's sprites
-marioAccelerate = 0.2
+marioAccelerate = 0.2 # The value at which mario can speed up and slow down
 backPos = 0  # Position of the background
 levelNum = 0  # Using 0 as level 1 since indexes start at 0
 isAnimating = False  # Boolean to see if we need to pause the screen and animate mario
@@ -80,11 +85,11 @@ def drawScene(background, backX, mario, marioPic, marioFrame, rectList, brickPic
     """Function to draw the background, mario, enemies, and all objects"""
     X, Y, VX, VY, DIR, STATE = 0, 1, 2, 3, 4, 5
     ONGROUND, JUMPFRAMES, INGROUND, ISCROUCH, ONPLATFORM, ISFALLING = 0, 1, 2, 3, 4, 5
-    screen.fill(BLACK)
-    screen.blit(background, (backX, 0))
+    screen.fill(BLACK) # Clearing screen
+    screen.blit(background, (backX, 0))  # Blitting background
     marioShow = marioPic[marioFrame[0]][int(marioFrame[1])]
     if mario[DIR] == "Left":
-        marioShow = transform.flip(marioShow, True, False)
+        marioShow = transform.flip(marioShow, True, False)  # Flipping mario's sprite if he's facing left
     for list in rectList:
         for brick in list:
             brickRect = Rect (brick[0], brick[1], brick[2], brick[3])
@@ -94,13 +99,14 @@ def drawScene(background, backX, mario, marioPic, marioFrame, rectList, brickPic
                 draw.rect(screen, BLUE, brickRect)
             else:
                 draw.rect(screen,GREEN,brickRect)
-    screen.blit(marioShow, (mario[0], mario[1]))
+    screen.blit(marioShow, (mario[0], mario[1])) # Blitting mario's sprite
 
 def drawPause():
     alphaSurface = Surface((800, 600))  # Making a surface
     alphaSurface.set_alpha(128)  # Giving it alpha functionality
     alphaSurface.fill((0, 0, 0))  # Fill the surface with a black background
     screen.blit(alphaSurface, (0, 0)) # Blit it into the actual screen
+    # Blitting text
     screen.blit(pauseText, (345,290))
     screen.blit(helpText, (210, 330))
 
@@ -109,19 +115,20 @@ def moveSprites(mario, marioInfo, marioPic, frame):
     X, Y, VX, VY, DIR, STATE = 0, 1, 2, 3, 4, 5
     ONGROUND, JUMPFRAMES, INGROUND, ISCROUCH, ONPLATFORM, ISFALLING = 0, 1, 2, 3, 4, 5
     if marioInfo[ONGROUND]:
-        frame[0] = 0 + mario[STATE]
+        frame[0] = 0 + mario[STATE] # Adjusting for sprite for = big mario
+        # Mario's running sprite counter
         if frame[1] < 3.8:
             frame[1] += mario[VX]**2/100 + 0.2
         else:
             frame[1] = 1
-        if frame[1] > 3.9:
+        if frame[1] > 3.9: # Sprite counter upper limit
             frame[1] = 3.9
-        if mario[VX] == 0:
+        if mario[VX] == 0: # If mario isn't moving, stay on his standing sprite
             frame[1] = 0
     else:
-        frame[0],frame[1] = 2, 0 + mario[STATE]
+        frame[0],frame[1] = 2, 0 + mario[STATE] # If mario is midair, stay on his jumping sprite
     if marioInfo[ISCROUCH]:
-        frame[0],frame[1] = 2, 2 
+        frame[0],frame[1] = 2, 2  # If mario is crouching, stay on his crouching sprite
 
 
 
@@ -132,9 +139,9 @@ def checkMovement(mario, marioInfo, acclerate, rectLists, pressSpace):
     ONGROUND, JUMPFRAMES, INGROUND, ISCROUCH, ONPLATFORM, ISFALLING = 0, 1, 2, 3, 4, 5
     moving = False
     # Walking logic
-    if keys[K_a] and keys[K_d]:
+    if keys[K_a] and keys[K_d]:  # If both keys are pressed, don't move
         mario[VX] = 0
-    elif keys[K_a] and not marioInfo[ISCROUCH]: # Checking if mario is hitting left side of window
+    elif keys[K_a] and not marioInfo[ISCROUCH]:  # Checking if mario is hitting left side of window
         if mario[DIR] != "Left":
             mario[VX] = 0  # Stop acceleration if changing direction
         walkMario(mario, rectLists, "Left")
@@ -142,25 +149,25 @@ def checkMovement(mario, marioInfo, acclerate, rectLists, pressSpace):
         mario[DIR] = "Left"
     elif keys[K_d] and not marioInfo[ISCROUCH]:
         if mario[DIR] != "Right":
-            mario[VX] = 0 # Stop acceleration if changing direction
+            mario[VX] = 0  # Stop acceleration if changing direction
         walkMario(mario, rectLists, "Right")
         moving = True
         mario[DIR] = "Right"
-    if keys[K_s] and mario[STATE]==1:
+    if keys[K_s] and mario[STATE]==1:  # Allow crouching if big mario is active
         marioInfo[ISCROUCH]=True
-    if mario[STATE]==0 and marioInfo[ISCROUCH]:
+    if mario[STATE]==0 and marioInfo[ISCROUCH]:  # Don't allow small mario to be in crouching position
         marioInfo[ISCROUCH]=False
-    if moving: # Accelerate if there is input
+    if moving:  # Accelerate if there is input
         if marioInfo[ONGROUND]:
             mario[VX] += acclerate
         else:
-            mario[VX] += acclerate/4 # Slow down movement when midair
-    elif mario[VX] != 0: # Move and decelerate if there is no input
+            mario[VX] += acclerate/4  # Slow down movement when midair
+    elif mario[VX] != 0:  # Move and decelerate if there is no input
         if mario[DIR] == "Right":
             walkMario(mario, rectLists, "Right")
         if mario[DIR] == "Left":
             walkMario(mario, rectLists, "Left")
-        if marioInfo[ONGROUND]: # Don't decelerate mid air
+        if marioInfo[ONGROUND]:  # Don't decelerate mid air
             mario[VX] -= acclerate
 
     # Max and min acceleration
@@ -172,21 +179,22 @@ def checkMovement(mario, marioInfo, acclerate, rectLists, pressSpace):
     gravity = 0.6
     floor=496
     marioOffset = 42
-    if mario[STATE]==1: # Change values if mario is big
+    if mario[STATE]==1:  # Change values if mario is big
         floor=452
         marioOffset = 88
-    if marioInfo[ISCROUCH]:
+    if marioInfo[ISCROUCH]:  # If mario is crouching, give him more gravity
         gravity = 0.9
 
-    if marioInfo[ONPLATFORM] and mario[VY] <= gravity*2 and pressSpace:
+    if marioInfo[ONPLATFORM] and mario[VY] <= gravity*2 and pressSpace:  # If mario is on a platform and pressing space, let him jump
         marioInfo[ISFALLING] = False
         marioInfo[ONPLATFORM] = False
 
     if keys[K_SPACE] and not marioInfo[ISCROUCH] and not marioInfo[ONPLATFORM]:
-        if marioInfo[ONGROUND] and pressSpace: # checking if jumping is true
-            mario[VY] -= 9.5 # jumping power
+        if marioInfo[ONGROUND] and pressSpace:  # Checking if jumping is true
+            mario[VY] -= 9.5  # Jumping power
             marioInfo[ONGROUND] = False
             marioInfo[JUMPFRAMES] = 0
+            # Playing jumping sounds
             if mario[STATE] == 0:
                 playSound("effects/smallJump.ogg", "effect")
             else:
@@ -202,14 +210,14 @@ def checkMovement(mario, marioInfo, acclerate, rectLists, pressSpace):
         # Using colour collision to fall through holes
         marioInfo[INGROUND] = True
         marioInfo[ONGROUND] = False
-    elif mario[Y] >= floor and not marioInfo[INGROUND]: # Checking floor collision
-        mario[Y] = floor # stay on the ground
-        mario[VY] = 0 # stop falling
+    elif mario[Y] >= floor and not marioInfo[INGROUND]:  # Checking floor collision
+        mario[Y] = floor  # stay on the ground
+        mario[VY] = 0  # stop falling
         marioInfo[ONGROUND] = True
         marioInfo[ONPLATFORM] = False
         marioInfo[ISFALLING] = False
 
-    marioPos[VY] += gravity # apply gravity
+    marioPos[VY] += gravity  # apply gravity
 
 
 def walkMario(mario, rectLists, direction):
@@ -247,34 +255,34 @@ def checkCollide(mario, marioInfo, rectLists):
             brickRect = Rect(brick[0], brick[1], brick[2], brick[3])
             marioRect = Rect(mario[X] + 2, mario[Y], 38 - 2, height) # Mario's hit box (and making it a little smaller)
             if brickRect.colliderect(marioRect):
-                if int(mario[Y]) + height - int(mario[VY]) <= brickRect.y:
+                if int(mario[Y]) + height - int(mario[VY]) <= brickRect.y:  # Hitting top collision
                     marioInfo[ONGROUND] = True
                     marioInfo[ONPLATFORM] = True
                     marioInfo[ISFALLING] = True
                     mario[VY] = 0
                     mario[Y] = brickRect.y - height
-                elif mario[Y] - mario[VY] >= brickRect.y + brickRect.height:
+                elif mario[Y] - mario[VY] >= brickRect.y + brickRect.height:  # Hitting bottom collision
                     mario[Y] -= mario[VY]
                     mario[VY] = 1
                     mario[Y] = brickRect.y + brickRect.height
                     marioInfo[JUMPFRAMES] = 41
-                    playSound("effects/bump.ogg", "effect")
-                elif mario[X] >= brickRect[X]: # and mario[DIR] == "Left":
-                    mario[X] = brickRect.x + brickRect.width - 2
+                    playSound("effects/bump.ogg", "effect")  # Play bumping sound
+                elif mario[X] >= brickRect[X]:  # Right side collision
+                    mario[X] = brickRect.x + brickRect.width - 2  # Move mario to the right of the rect
                     mario[VX] = 0
-                elif mario[X] <= brickRect[X]: # and mario[DIR] == "Right":
-                    mario[X] = brickRect.x - 38
+                elif mario[X] <= brickRect[X]:  # Left side collision
+                    mario[X] = brickRect.x - 38  # Move mario to the left of the rect
                     mario[VX] = 0
 
 def playSound(soundFile, soundChannel):
     """ Function to load in sounds and play them on a channel """
-    channelList = [["music", 0], ["effect", 1]]
-    for subList in channelList:
+    channelList = [["music", 0], ["effect", 1]]  # List to keep track of mixer channels
+    for subList in channelList:  # For loop to identify the input
         if subList[0] == soundChannel:
             channelNumber = subList[1]
-    soundObject = mixer.Sound("assets/music/" + soundFile)
-    mixer.Channel(channelNumber).stop()
-    mixer.Channel(channelNumber).play(soundObject)
+    soundObject = mixer.Sound("assets/music/" + soundFile)  # Loading the sound file
+    mixer.Channel(channelNumber).stop()  # Stopping any previous sound
+    mixer.Channel(channelNumber).play(soundObject)  # Playing new sound
 
 def globalSound(command):
     """ Function to apply commands to all mixer channels """
@@ -301,14 +309,14 @@ def cycleList(rectLists):
 def loadFile(targetFile):
     """ Function to load files and make lists out of them"""
     outputList = []
-    file = open(targetFile, "r")
-    fileLines = file.readlines()
+    file = open(targetFile, "r")  # Loading file
+    fileLines = file.readlines()  # Splitting into lines
     for line in fileLines:
-        line = line.strip("\n")
-        line = line.split(",")
+        line = line.strip("\n")  # Removing any line seperators
+        line = line.split(",")  # Dividing elements seperated by commas
         listLength = len(line)
-        outputList.append([int(line[index]) for index in range(listLength)])
-    return outputList
+        outputList.append([int(line[index]) for index in range(listLength)])  # Appending line info to list
+    return outputList  # Returning final list
 
 # Declaring main functions
 
@@ -317,9 +325,9 @@ def game():
     X, Y, VX, VY, DIR, STATE = 0, 1, 2, 3, 4, 5
     ONGROUND, JUMPFRAMES, INGROUND, ISCROUCH, ONPLATFORM, ISFALLING = 0, 1, 2, 3, 4, 5
     global marioStats, RECTFINDER, marioPos
-    playSound("songs/mainSong.ogg", "music")
+    playSound("songs/mainSong.ogg", "music")  # Starting the background music
     pausedBool = False
-    startTime = time.get_ticks()
+    startTime = time.get_ticks()  # Variable to keep track of time since level start
     while running:
         mx, my = mouse.get_pos()
         initialSpace = False
@@ -403,6 +411,7 @@ def menu():
 
 def loading():
     running = True
+    # Loading up and declaring all level elements
     global brickList, interactBricks, questionBricks, marioPos, backPos, marioStats
     marioPos = [40, 496, 0, 0, "Right", 0]
     marioStats = [True, 0, False, False, False, False]
@@ -415,6 +424,7 @@ def loading():
             if evnt.type == QUIT:
                 return "exit"
         if key.get_pressed()[K_RETURN]: running = False
+        screen.fill(BLACK)
         tempText = marioFont.render("LOADING...", False, (255,255,255))
         screen.blit(tempText,(0,0))
         display.flip()
@@ -445,7 +455,7 @@ def credit():
         fpsCounter.tick(60)
     return "menu"
 
-
+# Main loop to check for which page to fall on
 while page != "exit":
     if page == "menu":
         page = menu()
